4032d73b30d616baf986cccd01c4faab793fad2a
William Lafontaine <wil90_999@hotmail.com>
2018-03-22 20:25:50 -0400

    valider

--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
-using System.Net.Sockets;
-using System.Net;
 
 namespace Lab6
 {/*****
@@ -18,13 +9,16 @@ namespace Lab6
 *****/
     public partial class Form1 : Form
     {
+        //Déclaration du thread et le serveur
         Thread t;
         ServeurEcoute serveur;
         
         public Form1()
         {
             InitializeComponent();
+            //Crée la classe serveurecoute et autorise l'exécution de la boucle dans la classe
             serveur = new ServeurEcoute();
+            //Empêche la boucle de serveur écoute
             btnArret.Enabled = false;
         }
 
@@ -33,11 +27,10 @@ namespace Lab6
         {
             //Désactiver le bouton démarrer
             btnDemarrer.Enabled = false;
-
             //Instancier l'objet t pour démarrer la thread «MaThreadEcoute» de la classe ServeurEcoute
             t = new Thread(new ThreadStart(serveur.MaThreadEcoute));
+            //autorise la boucle de serveur écoute
             serveur.m_Fin = false;
-
             //Démarrer la thread
             t.Start();
             btnArret.Enabled = true;
@@ -45,6 +38,7 @@ namespace Lab6
 
         private void btnArret_Click(object sender, EventArgs e)
         {
+            //Empêche la continuation de la boucle de serveur écoute
             serveur.m_Fin = true;
             btnDemarrer.Enabled = true;
             btnArret.Enabled = false;
--- Lab6/ServeurEcoute.cs
+++ Lab6/ServeurEcoute.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 using System.Net.Sockets;
 using System.Net;
@@ -38,68 +35,80 @@ namespace Lab6
                 LeSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 LeSocket.Bind(PointLocal);
             }
+            //Si la connexion a des problèmes
             catch (SocketException ex)
             {
                 MessageBox.Show(ex.ToString());
                 return;
             }
 
-            //Boucle de la thread
-            while (!m_Fin)
+            try
             {
-                if (LeSocket.Available > 0)
+                //Boucle de la thread
+                while (!m_Fin)
                 {
-                    NbrRecu = LeSocket.ReceiveFrom(bTexte, ref PointDistant);
-
-                    sTexte = Encoding.ASCII.GetString(bTexte).Substring(0, NbrRecu);
-
-                    //Conversion bytes en string
-                    for (i = 2; bTexte[i] != 0; i++)
-                        bNomFich[i - 2] = bTexte[i];
-                    NomFichier = Encoding.ASCII.GetString(bNomFich).Substring(0, i - 2);
-                  
-
-                    switch (ValiderTrame(bTexte))
+                    if (LeSocket.Available > 0)
                     {
-                        //Pour obtenir le nom du fichier en bytes
-                        //Si le code est 0001, soit un RRQ
-                        case 1:
-                            RRQ rrq = new RRQ();
-                            rrq.SetPointDistant(PointDistant);
-                            rrq.SetFichier(NomFichier);
-                            LeThread = new Thread(new ThreadStart(rrq.MonThreadRRQ));
-                            LeThread.Start();
-                            break;
-                        //Sinon, si le code est 0002, soit un WRQ
-                        case 2:
-                            WRQ wrq = new WRQ();
-                            wrq.SetPointDistant(PointDistant);
-                            wrq.SetFichier(NomFichier);
-                            LeThread = new Thread(new ThreadStart(wrq.MonThreadWRQ));
-                            LeThread.Start();
-                            break;
-                        //Construit un message d'erreur
-                        case 0:
-                            bErreur[0] = 0x00;
-                            bErreur[1] = 0x05;
-                            bErreur[2] = 0x00;
-                            bErreur[3] = 0x04;
-                            MessageErreur = Encoding.ASCII.GetBytes("Opération TFTP illégale.");
-                            Buffer.BlockCopy(MessageErreur, 0, bErreur, 4, 1);
-                            bErreur[33] = 0x00;
-                            LeSocket.SendTo(bErreur, PointDistant); 
-                            break;
+                        //Reçois une trame
+                        NbrRecu = LeSocket.ReceiveFrom(bTexte, ref PointDistant);
+                        sTexte = Encoding.ASCII.GetString(bTexte).Substring(0, NbrRecu);
+                        //Conversion bytes en string du fichier
+                        for (i = 2; bTexte[i] != 0; i++)
+                            bNomFich[i - 2] = bTexte[i];
+                        NomFichier = Encoding.ASCII.GetString(bNomFich).Substring(0, i - 2);
+                        //Valide une trame et envoie le client au rrq ou wrq, sinon envoie une erreur au client
+                        switch (ValiderTrame(bTexte))
+                        {
+                            //Pour obtenir le nom du fichier en bytes
+                            //Si le code est 0001, soit un RRQ
+                            case 1:
+                                RRQ rrq = new RRQ();
+                                rrq.SetPointDistant(PointDistant);
+                                rrq.SetFichier(NomFichier);
+                                LeThread = new Thread(new ThreadStart(rrq.MonThreadRRQ));
+                                LeThread.Start();
+                                break;
+                            //Sinon, si le code est 0002, soit un WRQ
+                            case 2:
+                                WRQ wrq = new WRQ();
+                                wrq.SetPointDistant(PointDistant);
+                                wrq.SetFichier(NomFichier);
+                                LeThread = new Thread(new ThreadStart(wrq.MonThreadWRQ));
+                                LeThread.Start();
+                                break;
+                            //Construit un message d'erreur
+                            case 0:
+                                bErreur[0] = 0x00;
+                                bErreur[1] = 0x05;
+                                bErreur[2] = 0x00;
+                                bErreur[3] = 0x04;                                
+                                MessageErreur = Encoding.ASCII.GetBytes("Opération TFTP illégale.");
+                                Buffer.BlockCopy(MessageErreur, 0, bErreur, 4, 1);
+                                bErreur[33] = 0x00;
+                                //Envoie la trame erreur
+                                LeSocket.SendTo(bErreur, PointDistant);
+                                break;
+                        }
                     }
                 }
             }
-            LeSocket.Close();
+            //Si la connexion a des problèmes
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+            //Fini toujours par fermer le socket réseau
+            finally
+            {
+                LeSocket.Close();
+            }                
         }
-
-
         //Méthode qui valide la trame
         private int ValiderTrame(byte[] bTrame)
         {
             //Validation au complet de la trame 
+            //En retournant une valeur qui égal à wrq ou rrq, sinon envoie un 0 pour erreur
             if (bTrame[0] == 0 && bTrame[1] == 1 && bTrame[2] != 0)
                 return 1;
             else if (bTrame[0] == 0 && bTrame[1] == 2 && bTrame[2] != 0)
@@ -107,6 +116,5 @@ namespace Lab6
             else
                 return 0;
         }
-
     }
 }
